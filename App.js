bd81a0f0fb94bde7bfa77f3e8a9d524a3b9b7a6f
minhduongpro <duong1122334466@gmail.com>
2024-10-29 13:48:42 +0700

    ES6 Module

---
+++ App.js
@@ -0,0 +1,5 @@
+import react3, {func, value as value1} from "./React.js";
+
+console.log(react3)
+console.log(value1)
+func('ow ddaay')
---
+++ React.js
@@ -0,0 +1,11 @@
+const react = 'React'
+
+const value = 200
+
+function func(a){
+    console.log(a)
+}
+
+export {value, func}
+
+export default react
